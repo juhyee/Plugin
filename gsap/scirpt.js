47c0d04571faf3cb34274f501f38ba772d5c056c
juhyee <vlel3657@naver.com>
2023-06-26 17:21:38 +0900

    feat:  gsap plugin example

--- gsap/scirpt.js
+++ gsap/scirpt.js
@@ -40,3 +40,84 @@ document.querySelector("#pause").onclick = () => tween.pause();
 document.querySelector("#resume").onclick = () => tween.resume();
 document.querySelector("#reverse").onclick = () => tween.reverse();
 document.querySelector("#restart").onclick = () => tween.restart();
+
+
+gsap.registerPlugin(MotionPathPlugin);
+gsap.to(".ani03 .pink", {
+  duration: 5,
+  repeat: 12,
+  repeatDelay: 3,
+  yoyo: true,
+  ease: "power1.inOut",
+  motionPath:{
+    path: "#path",
+    align: "#path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5]
+  }
+});
+
+
+gsap.fromTo(".ani04 .orange", {rotation: -8},{rotation: 8, duration: 0.3, yoyo:true, ease: "none", repeat: -1 });
+gsap.fromTo(".ani04 .orange", {y: -30},{y: 30, duration: 0.6, yoyo:true,ease: "none", repeat: -1 });
+
+
+const t2 = gsap.timeline({repeat: 1, repeatDelay: 1});
+
+t2.fromTo($('.ani05 ._card_set'), {
+  opacity: 1,
+  rotate: -55,
+  x: -300,
+  y: 670,
+  scaleX: 0.5,
+  scaleY: 0.5,
+}, {
+  rotate: 0,
+  x: 0,
+  y: 0,
+  scaleX: 1,
+  scaleY: 1,
+  duration: 1.6,
+  ease: 'power1.out',
+}, '< 1')
+.fromTo($('.ani05 ._card_set > div'), {
+  rotate: -28,
+  skewY: 5,
+},{
+  rotate: 0,
+  skewY: 0,
+  duration: 0.8,
+  stagger: -0.2, //stagger: 시간차 부여
+  ease: 'power1.out',
+  onComplete: function() {
+
+      $('.ani05 ._card_set > div').each(function(i, v) {
+
+        var $this = $(this);
+        var $hover = $(this).find('img');
+        var animation = null;
+        var isHovering = false;
+
+        $this.on('mouseenter', function() {
+          $(this).addClass('_active');
+          gsap.to($(this).find('img'), {
+            y: -(120 - ((i*13)/0.9)),
+            ease: "power1.out",
+            duration: (+(Math.round(0.4 - (i*1.5/100) + 'e+2') + 'e-2')),
+            overwrite: true,
+          });
+
+        }).on('mouseleave', function() {
+          gsap.to($('.ani05 ._card_set > ._active img'), {
+            y: 0,
+            ease: "power1.out",
+            duration: (+(Math.round(0.4 - (i*1.5/100) + 'e+2') + 'e-2')),
+            overwrite: true,
+          });
+          $(this).removeClass('_active');
+        });
+
+      });
+
+  },
+})
