45e79251c39724468eacb5a718dcfa8cecca15a1
juhyee <vlel3657@naver.com>
2023-08-17 14:13:55 +0900

    feat: plugin common list

--- menu.js
+++ menu.js
@@ -1,28 +1,33 @@
-function loadItem(){
-  return fetch("../js/list.json")
-  .then((res) => res.json())
-  .then((json => json.listItem))
-}
+window.onload = function(){
 
-loadItem().then((items) => {
-  List(items)
-})
-
-
-function List(items){
-  let itemData = items.reverse()
-
-  let list = document.querySelector('.gnb_list')
-  itemData.forEach(item => {
-    list.insertAdjacentHTML('afterbegin', `
-      <li class="list_item"><a href="${item.url}">${item.title}</a></li>
-    `)
-  });
-
-
-  let close_btn = document.querySelector('.aside .close_btn')
-
-  close_btn.addEventListener('click', function(){
-      close_btn.parentNode.parentNode.classList.toggle('on')
+  function loadItem(){
+    return fetch("../js/list.json")
+    .then((res) => res.json())
+    .then((json => json.listItem))
+  }
+  
+  loadItem().then((items) => {
+    List(items)
   })
+  
+  
+  function List(items){
+    let itemData = items.reverse()
+  
+    let list = document.querySelector('.gnb_list')
+    itemData.forEach(item => {
+      list.insertAdjacentHTML('afterbegin', `
+        <li class="list_item"><a href="${item.url}">${item.title}</a></li>
+      `)
+    });
+  
+  
+    let close_btn = document.querySelector('.aside .close_btn')
+  
+    close_btn.addEventListener('click', function(){
+        close_btn.parentNode.parentNode.classList.toggle('on')
+    })
+  }
+
 }
+
